aebfe2629c2fd9f2375c17255485176768016a91
cyborg <hello@cleberg.io>
2020-08-13 14:23:32 -0700

    add container to navbar

--- index.php
+++ index.php
@@ -125,46 +125,48 @@
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-dark">
-        <a class="navbar-brand" href="#">Vox Populi</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+        <div class="container">
+            <a class="navbar-brand" href="#">Vox Populi</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-0 ml-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Filter Posts
-                    </a>
-                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="./?type=audio">Audio</a>
-                        <a class="dropdown-item" href="./?type=chat">Chat</a>
-                        <a class="dropdown-item" href="./?type=link">Link</a>
-                        <a class="dropdown-item" href="./?type=photo">Photo</a>
-                        <a class="dropdown-item" href="./?type=text">Text</a>
-                        <a class="dropdown-item" href="./?type=video">Video</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="./">All Posts</a>
-                    </div>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="https://github.com/christian-cleberg/vox-populi" target="_blank">GitHub <i data-feather="arrow-up-right"></i></a>
-                </li>
-            </ul>
-            <!--
-            <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-            </form>
-            -->
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-0 ml-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="./">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Filter Posts
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="./?type=audio">Audio</a>
+                            <a class="dropdown-item" href="./?type=chat">Chat</a>
+                            <a class="dropdown-item" href="./?type=link">Link</a>
+                            <a class="dropdown-item" href="./?type=photo">Photo</a>
+                            <a class="dropdown-item" href="./?type=text">Text</a>
+                            <a class="dropdown-item" href="./?type=video">Video</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="./">All Posts</a>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Profile</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/christian-cleberg/vox-populi" target="_blank">GitHub <i data-feather="arrow-up-right"></i></a>
+                    </li>
+                </ul>
+                <!--
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+                -->
+            </div>
         </div>
     </nav>
 
