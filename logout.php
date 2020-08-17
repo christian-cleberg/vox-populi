b9daa6c75903487d0b6c403d1fd5e4f7502d6344
cyborg <hello@cleberg.io>
2020-08-16 20:03:18 -0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -11,6 +11,17 @@ session_destroy();
 session_write_close();
 setcookie(session_name(),'',0,'/');
 
+// Unset cookies
+if (isset($_SERVER['HTTP_COOKIE'])) {
+    $cookies = explode(';', $_SERVER['HTTP_COOKIE']);
+    foreach($cookies as $cookie) {
+        $parts = explode('=', $cookie);
+        $name = trim($parts[0]);
+        setcookie($name, '', time()-1000);
+        setcookie($name, '', time()-1000, '/');
+    }
+}
+
 // Go back to sign-in page
 header('Location: https://cleberg.io/vox-populi/');
 die();
