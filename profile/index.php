872c2f338a9789c4390957946cbcc846eb44dfe3
cyborg <hello@cleberg.io>
2020-10-24 20:43:50 +0000

    update links to gitlab

--- profile/index.php
+++ profile/index.php
@@ -208,7 +208,7 @@
                             <hr class="sidebar-divider">
                             <ul class="meta">
                                 <li>
-                                    <a href="https://github.com/christian-cleberg/vox-populi" target="_blank">GitHub <svg
+                                    <a href="https://gitlab.com/christian-cleberg/vox-populi" target="_blank">GitLab <svg
                                             id="icon-external-link" xmlns="http://www.w3.org/2000/svg" width="16"
                                             height="16" viewBox="0 0 16 16" aria-hidden="true">
                                             <path
